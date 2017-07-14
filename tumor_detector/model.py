e00f74e9d657f74330a2f8ce8150cc08664cc214
Drew Erickson <drew.t.erickson@gmail.com>
2017-07-14 00:44:56 -0700

    Added function model_2d_with_drop.  Updated main block to use updated DataSet object and model_2d_with_drop.

--- tumor_detector/model.py
+++ tumor_detector/model.py
@@ -1,6 +1,6 @@
 import config
 from dataset import DataSet
-from keras.layers import Dense, Activation, Input, concatenate, BatchNormalization
+from keras.layers import Input, concatenate, BatchNormalization
 from keras.layers.convolutional import Conv3D, MaxPooling3D, UpSampling3D, Conv2D, MaxPooling2D, UpSampling2D
 from keras.layers.core import Dropout
 from keras.losses import binary_crossentropy
@@ -106,12 +106,67 @@ def model_2d(channels=4, categories=2, optimizer=Adam()):
     return model
 
 
+def model_2d_with_drop(channels=4, categories=2, optimizer=Adam()):
+
+    layer00 = Input(shape=(240, 240, channels))
+    layer00_2 = BatchNormalization()(layer00)
+
+    layer01 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(layer00_2)
+    layer02 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer01)
+    layer03 = Dropout(0.5)(layer02)
+
+    layer04 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(layer03)
+    layer05 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer04)
+    layer06 = Dropout(0.4)(layer05)
+
+    layer07 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(layer06)
+    layer08 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer07)
+    layer09 = Dropout(0.3)(layer08)
+
+    layer10 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(layer09)
+    layer11 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer10)
+    layer12 = Dropout(0.2)(layer11)
+
+    layer13 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(layer12)
+    layer14 = UpSampling2D(size=(2, 2))(layer13)
+    layer15 = Dropout(0.2)(layer14)
+
+    concat01 = concatenate([layer10, layer15])
+
+    layer16 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(concat01)
+    layer17 = UpSampling2D(size=(2, 2))(layer16)
+    layer18 = Dropout(0.2)(layer17)
+
+    concat02 = concatenate([layer07, layer18])
+
+    layer19 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(concat02)
+    layer20 = UpSampling2D(size=(2, 2))(layer19)
+    layer21 = Dropout(0.2)(layer20)
+
+    concat03 = concatenate([layer04, layer21])
+
+    layer22 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(concat03)
+    layer23 = UpSampling2D(size=(2, 2))(layer22)
+    layer24 = Dropout(0.2)(layer23)
+
+    concat04 = concatenate([layer01, layer24])
+
+    layer25 = Conv2D(64, (3, 3), activation='relu', border_mode='same')(concat04)
+    layer26 = Conv2D(categories, (1, 1), activation='sigmoid', border_mode='same')(layer25)
+
+    model = Model(layer00, layer26)
+
+    model.compile(loss=binary_crossentropy, optimizer=optimizer, metrics=['accuracy'])
+
+    return model
+
+
 if __name__ == '__main__':
 
     print("Job Started!")
 
-    model_id = "2000-005"
-    local = False
+    model_id = "2000-drop-000"
+    local = True
     set_optimizer = "adam"
     model_to_test = "2D"
     num_cats = 4
@@ -125,50 +180,45 @@ if __name__ == '__main__':
         ds = DataSet(bucket_name=config.bucket_name, prefix_folder=config.prefix_folder)
     print("Done.")
 
+    print("Loading Data... ", end="", flush=True)
+    ds.load_dataset(config.pids_of_interest)
+    print("Done.")
+
+    print("Train / Test Split... ", end="", flush=True)
+    X_train, X_test, y_train, y_test = ds.train_test_split()
+    print("Done.")
+
     print("Selecting Optimizer... ", end="", flush=True)
     optimizer = None
     if set_optimizer == "sgd":
         optimizer = SGD(lr=0.00001, momentum=0.9, nesterov=True)
     elif set_optimizer == "adam":
-        optimizer = Adam(lr=0.0001, decay=0.001)
+        optimizer = Adam(lr=0.001, decay=0.001)
     print("Done.")
 
-    print("Loading Data...", end="", flush=True)
-    if model_to_test == "2D":
-        ds.load_dataset()
-    elif model_to_test == "3D":
-        ds.load_dataset()
-    print("Done.")
-
-    print("Train: ", len(ds.index_train))
-    print("Test:  ", len(ds.index_test))
-    print("Total: ", len(ds.X))
-    print("Files: ", ds.y_keys)
-    print("Test Files: ", [ds.y_keys[i] for i in ds.index_test])
+    print("Train: ", len(X_train))
+    print("Test:  ", len(X_test))
+    print("Total: ", len(X_train) + len(X_test))
+    print("Test Patients: ", ds.test_patients)
 
     print("Initializing Model... ", end="", flush=True)
     model = None
     if model_to_test == "2D":
         model = model_2d(categories=num_cats, optimizer=optimizer)
-    elif model_to_test == "3D":
-        model = model_3d(categories=num_cats, optimizer=optimizer)
+    elif model_to_test == "2D_drop":
+        model = model_2d_with_drop(categories=num_cats, optimizer=optimizer)
     print("Done.")
 
     if train:
         print(model.summary())
         print("Training Model:")
-        model.fit(ds.X_train(), ds.y_train(), epochs=1000, validation_data=(ds.X_test(), ds.y_test()))
+        model.fit(X_train, y_train, epochs=3, validation_data=(X_test, y_test))
         print("Done.")
     else:
         model.load_weights("model-" + model_id + ".h5")
 
-    print("Losses: ", model.losses)
-    print("Test: ", ds.index_test)
-    score = model.evaluate(ds.X_test(), ds.y_test())
-    print("Score: ", score)
-
     print("Building Prediction... ", end="", flush=True)
-    ds.save_y_predict(model, model_id)
+    ds.predict(model, model_id)
     print("Done.")
 
     print("Saving Model... ", end="", flush=True)
@@ -179,7 +229,3 @@ if __name__ == '__main__':
     print("Done.")
     print("")
     print("Job Complete.")
-
-#    loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-#    classes = model.predict(x_test, batch_size=128)
-
